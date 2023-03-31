0cedfbd94158dbfc0f6600b6d0336a70c31177b3
jeongSWsw <jeongsw34@gmail.com>
2023-03-31 18:16:45 +0900

    lotto

--- day05_random/src/quiz/Lotto.java
+++ day05_random/src/quiz/Lotto.java
@@ -11,19 +11,19 @@ public class Lotto {
 		for (int i = 1; i <= 5; i++) {
 			for (int j = 0; j < 6; j++) {
 				number[j] = rand.nextInt(45) + 1;
-				System.out.print(number[j] + " ");
 
 				// 중복 체크 start
-				for (int k = 0; k < 6; k++) {
-					for (int l = k+1; l < 6; l++) {
-						if (number[k] == number[l]) {
+				//for (int k = 0; k < 6; k++) {
+					for (int l = 0; l < j; l++) {
+						if (number[j] == number[l]) {
 							do {
-								number[k] = rand.nextInt(45) + 1;
-							} while (number[k] == number[l]);
+								number[j] = rand.nextInt(45) + 1;
+							} while (number[j] == number[l]);
 						}
-					}
+				//	}
 				}
 				// 중복 체크 end
+				System.out.print(number[j] + " ");
 
 			} // j end // 6개 숫자 출력
 			System.out.println();
