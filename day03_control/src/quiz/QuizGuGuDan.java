de9638dfaf5272eaba6ffbda225f1957948c3740
jeongSWsw <jeongsw34@gmail.com>
2023-03-31 10:37:25 +0900

    for2

---
+++ day03_control/src/quiz/QuizGuGuDan.java
@@ -0,0 +1,15 @@
+package quiz;
+
+public class QuizGuGuDan {
+	public static void main(String[] args) {
+
+		// 구구단 전체 출력 
+		for(int i = 1; i <= 9 ; i++) {
+			for(int j = 2; j <= 9; j++) {
+				System.out.printf("%d * %d = %d\t", j, i ,i*j);
+				
+			}
+			System.out.println();
+		}
+	}
+}
