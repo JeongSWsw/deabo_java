25c18bbe0eebf6dd22058d05d4ab13cfe48a93c7
jeongSWsw <jeongsw34@gmail.com>
2023-04-05 12:17:07 +0900

    overload

---
+++ day08_method/src/ex03/overload/MainEntry.java
@@ -0,0 +1,42 @@
+package ex03.overload;
+
+import java.util.Iterator;
+
+public class MainEntry {
+	
+	public static void line() {
+		System.out.println("----------------------------");
+	}
+	public static void line(int n,String str) {
+		// array : length 필드 <- 배열의 길이(크기)
+		// String : length 함수 <- 문자열의 길이
+		for(int i = 0; i < n; i++) {
+			System.out.print(str);
+		}
+		System.out.println();
+	}
+	
+	public static void main(String[] args) {
+		line(10,"*");
+		plus(12.34);
+		line(20, "A");
+		plus(20);
+		line();
+		plus("hello");
+		line();
+		
+	}
+
+	private static void plus(int x) {
+		System.out.println(x + 100);
+	}
+	private static void plus(int x, int y) {
+		System.out.println(x + y);
+	}
+	private static void plus(double x) {
+		System.out.println(x + 100);
+	}
+	private static void plus(String x) {
+		System.out.println(x + 100);
+	}
+}
