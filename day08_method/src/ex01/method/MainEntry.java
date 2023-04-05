4b143cb8525ef0937308de8e8ff70b6ed68acd5e
jeongSWsw <jeongsw34@gmail.com>
2023-04-05 09:49:06 +0900

    method2

---
+++ day08_method/src/ex01/method/MainEntry.java
@@ -0,0 +1,21 @@
+package ex01.method;
+
+public class MainEntry {
+	public static void main(String[] args) {
+		System.out.println("main start");
+		info();
+		System.out.println("main end");
+	}
+	
+//	1) 매개변수 X, 리턴타입 X
+	public static void info() {
+		System.out.println("doyeon");
+		System.out.println("010-9872-0202");
+		System.out.println("seoul");
+	}
+//	2) 매개변수 O, 리턴타입 X
+
+//	3) 매개변수 X, 리턴타입 O
+
+//	4) 매개변수 O, 리턴타입 O
+}
